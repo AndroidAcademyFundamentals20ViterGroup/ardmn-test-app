9f1c4d4eac18ed3f808ef878f5824b4b097bbd5e
Artem Viter <artiom.viter@gmail.com>
2020-11-23 02:07:20 +0300

    Adds samples with using custom fonts.
    The result of running app on device with api level
    21 and api level 26+ will be different because android
    with api level lees then 26 dont support font-family
    which declared in xml files(like /res/font/roboto_bold_family.xml).

--- app/src/main/java/com/viter/artem/aa_2020_viter_group_test_app/MainActivity.kt
+++ app/src/main/java/com/viter/artem/aa_2020_viter_group_test_app/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.viter.artem.aa_2020_viter_group_test_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // fonts: https://fonts.google.com/specimen/Roboto#standard-styles
+        // fonts: https://developer.android.com/guide/topics/ui/look-and-feel/fonts-in-xml
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
