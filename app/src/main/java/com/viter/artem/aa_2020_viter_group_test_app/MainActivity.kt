4d90945dbe733f518694998b7e771e9465016e52
Artem Viter <artiom.viter@gmail.com>
2020-11-23 03:42:07 +0300

    Add tricky sample for text with gradient-color by using WebView.

--- app/src/main/java/com/viter/artem/aa_2020_viter_group_test_app/MainActivity.kt
+++ app/src/main/java/com/viter/artem/aa_2020_viter_group_test_app/MainActivity.kt
@@ -4,6 +4,7 @@ import android.graphics.Color
 import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.os.Bundle
+import android.webkit.WebView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -20,6 +21,36 @@ class MainActivity : AppCompatActivity() {
                 0f, 0f, 0f, 35f,
                 Color.RED, Color.BLUE,
                 Shader.TileMode.CLAMP
+            )
+        }
+
+        findViewById<WebView>(R.id.webview).apply {
+            //setBackgroundColor(Color.parseColor("#33ff0000"))
+            loadData(
+                """
+                    <!DOCTYPE html>
+                    <html>
+                     <head>
+                      <meta charset="utf-8">
+                      <title>Градиентный текст</title>
+                      <style>
+                       body {
+                        background: transparent; /* Цвет фона веб-страницы */
+                       }
+                       p{
+                        background: linear-gradient(to right, orange, white); /* Градиент для текста */
+                        -webkit-background-clip: text; /* Обрезаем фон по тексту */
+                        -webkit-text-fill-color: transparent; /* Прозрачный текст */
+                       }
+                      </style>
+                     </head>
+                     <body>
+                      <p>Градиентный текст</p>
+                     </body> 
+                    </html>
+                """,
+                "text/html; charset=utf-8",
+                "UTF-8"
             )
         }
     }
