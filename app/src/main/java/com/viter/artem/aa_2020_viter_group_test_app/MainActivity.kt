3eb8acd349bdd59a33dcddc7ac7932484122a123
Artem Viter <artiom.viter@gmail.com>
2020-11-23 03:07:18 +0300

    Adds simple gradient sample by setting to TextView's paint hadcoded shader as LinearGradient.

--- app/src/main/java/com/viter/artem/aa_2020_viter_group_test_app/MainActivity.kt
+++ app/src/main/java/com/viter/artem/aa_2020_viter_group_test_app/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.viter.artem.aa_2020_viter_group_test_app
 
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,14 @@ class MainActivity : AppCompatActivity() {
         // fonts: https://developer.android.com/guide/topics/ui/look-and-feel/fonts-in-xml
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // simple gradient sample
+        findViewById<TextView>(R.id.gradient_text_view).paint.apply {
+            shader = LinearGradient(
+                0f, 0f, 0f, 35f,
+                Color.RED, Color.BLUE,
+                Shader.TileMode.CLAMP
+            )
+        }
     }
 }
